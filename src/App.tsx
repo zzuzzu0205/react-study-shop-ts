4cf1faa9709388dd27f203fb9e23ca413557ed8d
zzuzzu0205 <zkakcnd@naver.com>
2023-02-16 18:17:27 +0900

    'component'

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import data from "./data";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar bg="white" variant="white" className="mainBar">
+        <Container className="mainBarItm">
+          <Navbar.Brand href="#">SAMSUNG</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#">제품</Nav.Link>
+            <Nav.Link href="#">이벤트선물하기</Nav.Link>
+            <Nav.Link href="#">갤럭시캠퍼스스토어</Nav.Link>
+            <Nav.Link href="#">e-식품관</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+      <Item></Item>
+    </div>
+  );
+}
+function Item() {
+  let [item] = useState<any>(data);
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          {item.map(function (a: number, i: number) {
+            return (
+              <div className="col-md-4">
+                <img
+                  src={process.env.PUBLIC_URL + "/" + i + ".png"}
+                  width="80%"
+                />
+                <h4>{item[i].title}</h4>
+                <p>{item[i].content}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/data.ts
@@ -0,0 +1,23 @@
+let data: any[] = [
+  {
+    id: 0,
+    title: "Galaxy Z Flip4",
+    content: "Style and function that fits in your pocket",
+    price: 1119000,
+  },
+  {
+    id: 1,
+    title: "Galaxy Z Fold4",
+    content: "Lighter. More durable And now with our most powerful processor.",
+    price: 1639000,
+  },
+  {
+    id: 2,
+    title: "Galaxy S22",
+    content:
+      "Feast your eyes on the Galaxy S22 Ultra. Slim silhouette. Gorgeous colors. Mirrored lens ring. A polished elegant frame. Oh. So. Beautiful.",
+    price: 834000,
+  },
+];
+
+export default data;
