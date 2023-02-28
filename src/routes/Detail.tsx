ca20258abe9b86c602d454aa147c83b3b763e290
zzuzzu0205 <zkakcnd@naver.com>
2023-02-28 20:48:00 +0900

    'useEffect'

--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { Routes, Route, Link, useParams } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,7 +8,9 @@ import styled from "styled-components";
 // 변수 지정
 import { Item } from "../App";
 import { Style } from "util";
+import { Alert } from "react-bootstrap";
 
+//스타일 컴포넌트
 let YellowBtn = styled.button<any>`
   background: ${(props) => props.bg};
   color: ${(props) => (props.bg == "blue" ? "white" : "black")}
@@ -16,6 +18,20 @@ let YellowBtn = styled.button<any>`
 `;
 
 function Detail({ item }: { item: Item[] }) {
+  let [alert1, setAlert] = useState<boolean>(true);
+  let [count, setCount] = useState<number>(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+  }, [alert1]);
+
+  useEffect(() => {
+    if (isNaN(count) == true) {
+      alert("숫자만 입력하세요");
+    }
+  }, [count]);
+
   let { id } = useParams() as { id: string };
   let searchId = item.find(function (x) {
     return x.id == parseInt(id);
@@ -25,6 +41,9 @@ function Detail({ item }: { item: Item[] }) {
     <div className="container">
       <YellowBtn bg="blue">버튼</YellowBtn>
       <YellowBtn bg="yellow">버튼</YellowBtn>
+      {alert1 == true ? (
+        <div className="alert alert-warning">2초이내 구매시 할인</div>
+      ) : null}
 
       <div className="row">
         <div className="col-md-6">
@@ -40,6 +59,11 @@ function Detail({ item }: { item: Item[] }) {
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+      <input
+        onChange={(e) => {
+          setCount(parseInt(e.target.value));
+        }}
+      />
     </div>
   );
 }
